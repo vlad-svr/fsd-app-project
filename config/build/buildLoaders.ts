090effb5eab8a481d8d6670966e810664a39faec
Vladislav Sviridov <v_l_a_d_i_k1@mail.ru>
2023-09-05 23:23:48 +0300

    Add HotReload plugin, extract i18n plugin and babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -15,6 +15,21 @@ export function buildLoaders({isDev}: BuildOptions): RuleSetRule[] {
     use: ['@svgr/webpack'],
   };
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          isDev && require.resolve('react-refresh/babel'),
+          ['i18next-extract', {locales: ['ru', 'en']}],
+        ].filter(Boolean),
+      },
+    },
+  };
+
   const imagesLoader = {
     test: /\.(png|jpg|gif)$/i,
     type: 'asset/resource',
@@ -39,5 +54,5 @@ export function buildLoaders({isDev}: BuildOptions): RuleSetRule[] {
     ],
   };
 
-  return [imagesLoader, svgLoader, typescriptLoader, cssLoader];
+  return [imagesLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import webpack from 'webpack';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import {BuildOptions} from './types/config';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 
 export function buildPlugins({
   paths,
@@ -17,5 +18,7 @@ export function buildPlugins({
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-  ];
+    isDev && new ReactRefreshWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ].filter(Boolean);
 }

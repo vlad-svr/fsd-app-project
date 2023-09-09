6ae0773184069a507a1fd0f65acede029e10fcfa
Vladislav Sviridov <v_l_a_d_i_k1@mail.ru>
2023-09-09 12:12:38 +0300

    Fix jest environment

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,11 +1,16 @@
 import { render, screen } from '@testing-library/react'
-import { Button } from './Button'
+import { Button, ButtonTheme } from './Button'
 
 describe(
   'Button', () => {
-    test('', () => {
+    test('check if the button exists', () => {
       render(<Button>TEST</Button>)
       expect(screen.getByText('TEST')).toBeInTheDocument()
+    })
+
+    test('check if the button has pure theme', () => {
+      render(<Button theme={ButtonTheme.PURE}>TEST</Button>)
+      expect(screen.getByText('TEST')).toHaveClass('clear')
     })
   }
 )

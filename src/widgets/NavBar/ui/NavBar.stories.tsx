1b9ac31600bf56c36180026112716886cc230f42
Vladislav Sviridov <v_l_a_d_i_k1@mail.ru>
2023-09-18 19:23:09 +0300

    Add stories for the Navbar

--- src/widgets/NavBar/ui/NavBar.stories.tsx
+++ src/widgets/NavBar/ui/NavBar.stories.tsx
@@ -3,6 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { NavBar } from './NavBar'
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
 import { Theme } from 'app/providers/ThemeProvider'
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator'
 
 const meta = {
   title: 'Widget/NavBar',
@@ -16,4 +17,12 @@ export const Light: Story = {}
 
 export const Dark: Story = {
   decorators: [ThemeDecorator(Theme.DARK)]
+}
+
+export const Authorized: Story = {
+  decorators: [StoreDecorator({ user: { authData: {} } })]
+}
+
+export const AuthorizedDark: Story = {
+  decorators: [ThemeDecorator(Theme.DARK), StoreDecorator({ user: { authData: {} } })]
 }

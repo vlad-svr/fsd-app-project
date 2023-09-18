9c428ac3db1ce92553f60dd89f9cd06abf38071b
Vladislav Sviridov <v_l_a_d_i_k1@mail.ru>
2023-09-18 14:37:49 +0300

    Fix global themes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,12 @@
 import { Suspense } from 'react'
-import { useTheme } from 'app/providers/ThemeProvider'
 import classNames from 'shared/lib/classNames/classNames'
 import { AppRouter } from 'app/providers/router'
 import { NavBar } from 'widgets/NavBar'
 import { Sidebar } from 'widgets/Sidebar'
 
 function App () {
-  const { theme } = useTheme()
-
   return (
-      <div className={classNames('app', {}, [theme])}>
+      <div className={classNames('app', {})}>
           <Suspense fallback="">
               <NavBar />
               <div className="content-app">

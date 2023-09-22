ecbf6a41b90660503221b8cd06d893de211bb444
Vladislav Sviridov <v_l_a_d_i_k1@mail.ru>
2023-09-22 22:23:02 +0300

    Fix UI tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -3,6 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Sidebar } from './Sidebar'
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
 import { Theme } from 'app/providers/ThemeProvider'
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator'
 
 const meta = {
   title: 'Widget/Sidebar',
@@ -14,8 +15,27 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Light: Story = {}
+export const Light: Story = {
+  decorators: [
+    StoreDecorator({
+      user: { authData: {} }
+    })
+  ]
+}
 
 export const Dark: Story = {
-  decorators: [ThemeDecorator(Theme.DARK)]
+  decorators: [
+    ThemeDecorator(Theme.DARK),
+    StoreDecorator({
+      user: { authData: {} }
+    })
+  ]
+}
+
+export const NoAuth: Story = {
+  decorators: [
+    StoreDecorator({
+      user: { }
+    })
+  ]
 }

2fa79cade7268944de27d023f952b64eb81949b5
v.sviridov <v_l_a_d_i_k1@mail.ru>
2024-03-19 00:11:14 +0300

    Update Icons

--- config/build/loaders/buildSvgLoader.ts
+++ config/build/loaders/buildSvgLoader.ts
@@ -1,7 +1,22 @@
 function buildSvgLoader () {
   return {
     test: /\.svg$/i,
-    use: ['@svgr/webpack'],
+    use: [{
+      loader: '@svgr/webpack',
+      options: {
+        icon: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'convertColors',
+              params: {
+                currentColor: true
+              }
+            }
+          ]
+        }
+      }
+    }],
     exclude: /node_modules/
   }
 }
--- src/features/ThemeSwitcher/ui/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/features/ThemeSwitcher/ui/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,19 +1,18 @@
 import { memo } from 'react';
 import classNames from '@/shared/lib/classNames/classNames';
-import LightIcon from '@/shared/assets/icons/theme-light.svg';
-import DarkIcon from '@/shared/assets/icons/theme-dark.svg';
+import ThemeIcon from '@/shared/assets/icons/theme-light.svg';
 import { Button } from '@/shared/ui/Button';
-import { Theme } from '@/shared/constants/theme';
 import { useTheme } from '@/shared/lib/hooks/useTheme';
 import { saveJsonSettings } from '@/entities/User';
 import { useAppDispatch } from '@/shared/lib/hooks';
+import { Icon } from '@/shared/ui/Icon';
 
 interface ThemeSwitcherProps {
   className?: string;
 }
 
 const ThemeSwitcher = memo(({ className }: ThemeSwitcherProps) => {
-  const { theme, toggleTheme } = useTheme();
+  const { toggleTheme } = useTheme();
   const dispatch = useAppDispatch();
 
   const onToggleHandler = () => {
@@ -24,7 +23,7 @@ const ThemeSwitcher = memo(({ className }: ThemeSwitcherProps) => {
 
   return (
     <Button onClick={onToggleHandler} className={classNames('', {}, [className])}>
-      {theme === Theme.LIGHT ? <LightIcon /> : <DarkIcon />}
+      <Icon Svg={ThemeIcon} width={40} height={40} inverted />
     </Button>
   );
 });
